4da6011a10d5d9996a5f8b7c08295327cacd0f9b
xamuron <xamuron@gmail.com>
2020-10-04 14:10:44 +0300

    Init commit

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+
+public class App {
+    public static void main(String[] args) throws TelegramApiRequestException {
+        ApiContextInitializer.init();
+
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        botsApi.registerBot(new SimpleInvestBot());
+    }
+}
---
+++ src/main/java/SimpleInvestBot.java
@@ -0,0 +1,82 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleInvestBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotToken() {
+        return System.getProperty("token");
+    }
+
+    public void onUpdateReceived(Update update) {
+        try {
+            //проверяем есть ли сообщение и текстовое ли оно
+            if (update.hasMessage() && update.getMessage().hasText()) {
+                Message inputMessage = update.getMessage();
+                System.out.println("User message: " + inputMessage.getText());
+                if (inputMessage.getText().equals("/start")) {
+                    SendMessage message = new SendMessage();
+                    message
+                            .setChatId(inputMessage.getChatId())
+                            .setText("Hello! It's simple invest bot\n Press the button to continue");
+
+                    message.setReplyMarkup(setInlineDefaultKeyboard());
+                    execute(message);
+                }
+            } else
+                if (update.hasCallbackQuery()) {
+                    String callbackId = update.getCallbackQuery().getId();
+                    String msg1 = "The right choice, grats!";
+                    String msg2 = "It's wrong button, sorry!";
+
+                    String clb = update.getCallbackQuery().getData();
+
+                    if (clb.equals("button_1"))
+                        answerCallbackQuery(callbackId, msg1);
+                    else
+                        answerCallbackQuery(callbackId, msg2);
+
+                    System.out.println("Callback: " + clb);
+                }
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void answerCallbackQuery(String callbackId, String message) {
+        AnswerCallbackQuery answer = new AnswerCallbackQuery();
+        answer.setCallbackQueryId(callbackId);
+        answer.setText(message);
+        answer.setShowAlert(true);
+        try {
+            execute(answer);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private InlineKeyboardMarkup setInlineDefaultKeyboard() {
+        List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
+        List<InlineKeyboardButton> buttons1 = new ArrayList<>();
+        buttons1.add(new InlineKeyboardButton().setText("Button1").setCallbackData("button_1"));
+        buttons1.add(new InlineKeyboardButton().setText("Button2").setCallbackData("button_2"));
+        buttons.add(buttons1);
+
+        InlineKeyboardMarkup markupKeyboard = new InlineKeyboardMarkup();
+        markupKeyboard.setKeyboard(buttons);
+        return markupKeyboard;
+    }
+
+    public String getBotUsername() {
+        return "simple_fat_invest_bot";
+    }
+}
